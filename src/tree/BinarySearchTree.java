96b948aaf6d76f7a28d36dccb0dc887ff1fe22a3
dibyajyoti_behera <dibyajyoti_behera@dellteam.com>
2021-04-23 23:18:24 +0530

    91 completed

--- src/tree/BinarySearchTree.java
+++ src/tree/BinarySearchTree.java
@@ -1,4 +1,41 @@
 package tree;
 
-public class BinarySearchTree {
+
+
+public class BinarySearchTree<T extends Comparable<T>> {
+
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.insert(25);
+        tree.insert(24);
+        tree.insert(26);
+        tree.insert(28);
+        tree.insert(19);
+        System.out.println(tree);
+    }
+
+    private Node<T> root;
+
+    public void insert (T data) {
+        if (root == null) root = new Node<>(data);
+        else root.insert(data);
+    }
+
+    private class Node<T extends Comparable<T>> {
+        private T data;
+        private Node<T> leftChild;
+        private Node<T> rightChild;
+        Node(T data) { this.data = data;}
+        void insert (T data) {
+            if (data.compareTo(this.data) == 0) return; //no operation
+
+            if (data.compareTo(this.data) < 0) {
+                if (leftChild == null) leftChild = new Node<>(data);
+                else leftChild.insert(data);
+            } else {
+                if (rightChild == null) rightChild = new Node<>(data);
+                else rightChild.insert(data);
+            }
+        }
+    }
 }
