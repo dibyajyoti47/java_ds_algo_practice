81139e341f0415afca096a874a9ac0b733373301
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2021-04-25 11:01:08 +0530

    93 completed

--- src/tree/BinarySearchTree.java
+++ src/tree/BinarySearchTree.java
@@ -1,17 +1,25 @@
 package tree;
 
 
+import java.util.Optional;
 
 public class BinarySearchTree<T extends Comparable<T>> {
 
     public static void main(String[] args) {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
         tree.insert(25);
-        tree.insert(24);
+        tree.insert(20);
+        tree.insert(15);
+        tree.insert(27);
+        tree.insert(30);
+        tree.insert(29);
         tree.insert(26);
-        tree.insert(28);
-        tree.insert(19);
-        System.out.println(tree);
+        tree.insert(22);
+        tree.insert(32);
+        tree.traverseInOrder();
+        System.out.println(tree.isPresent(154));
+        System.out.println(tree.getMin());
+        System.out.println(tree.getMax());
     }
 
     private Node<T> root;
@@ -21,13 +29,33 @@ public class BinarySearchTree<T extends Comparable<T>> {
         else root.insert(data);
     }
 
+    public boolean isPresent (T data) {
+        if (root != null) return root.get(data) == null ? false : true;
+        return false;
+    }
+
+    public T getMin () {
+        if (root == null) return null;
+        else return root.getMin();
+    }
+
+    public T getMax () {
+        if (root == null) return null;
+        else return root.getMax();
+    }
+
+    public void traverseInOrder () {
+        if (root != null) root.traverseInOrder();
+        System.out.println();
+    }
+
     private class Node<T extends Comparable<T>> {
         private T data;
         private Node<T> leftChild;
         private Node<T> rightChild;
-        Node(T data) { this.data = data;}
-        void insert (T data) {
-            if (data.compareTo(this.data) == 0) return; //no operation
+        private Node(T data) { this.data = data;}
+        private void insert (T data) {
+            if (data.equals(this.data)) return; //no operation
 
             if (data.compareTo(this.data) < 0) {
                 if (leftChild == null) leftChild = new Node<>(data);
@@ -37,5 +65,31 @@ public class BinarySearchTree<T extends Comparable<T>> {
                 else rightChild.insert(data);
             }
         }
+        private Node<T> get (T data) {
+            if (this.data.equals(data)) return this;
+            if (data.compareTo(this.data) < 0) {
+                if (leftChild != null) return leftChild.get(data);
+            }
+            else {
+                if ( rightChild!=null ) return rightChild.get(data);
+            }
+            return null;
+        }
+        private T getMin () {
+            if (leftChild == null) return this.data;
+            else return leftChild.getMin();
+        }
+        private T getMax () {
+            if (rightChild == null) return this.data;
+            else return rightChild.getMax();
+        }
+        public void traverseInOrder() {
+//            System.out.print(this.data + " , ");  //pre-order
+            if (leftChild != null) leftChild.traverseInOrder();
+            System.out.print(this.data + " , ");
+            if (rightChild != null) rightChild.traverseInOrder();
+//            System.out.print(this.data + " , "); //post-order
+        }
+
     }
 }
